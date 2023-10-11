853ca26555f4e66ed03e8ad14bc1d92c9aad8d2a
mkrass1988 <krassmatthew@gmail.com>
2023-10-11 12:04:30 -0400

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,202 @@
+import { Link } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react';
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+function Navbar() {
+
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+    const signOutOnClick = () => {
+        logout();
+    };
+
+    const signInOnClick = () => {
+        loginWithRedirect();
+    };
+
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+            <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+                }}
+            >
+                <i className="fa-regular fa-futbol">The Soccer Site</i>
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+                >
+                <MenuIcon />
+                </IconButton>
+                <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                    display: { xs: 'block', md: 'none' },
+                }}
+                >
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                            <Link to='/'>Home</Link>
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                            <Link to='/about'>About</Link>
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                            <Link to='/contact'>Contact</Link>
+                        </Typography>
+                    </MenuItem>
+                </Menu>
+            </Box>
+            <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+                }}
+            >
+                <i className="fa-regular fa-futbol">The Soccer Site</i>
+            </Typography>
+            
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }}}>
+                <Button
+                    key='home'
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                    <Link to="/">Home</Link>
+                </Button>
+                <Button
+                    key='about'
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                    <Link to="/about">About</Link>
+                </Button>
+                <Button
+                    key='Contact'
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                    <Link to="/contact">Contact</Link>
+                </Button>
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="" src="" />
+                </IconButton>
+                </Tooltip>
+                <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+                >
+                    { !isAuthenticated ? 
+                    <MenuItem onClick={signInOnClick}>
+                        <Typography textAlign="center">Login</Typography>
+                    </MenuItem>:
+                    [
+                    <MenuItem onClick={signOutOnClick}>
+                        <Typography textAlign="center">Logout</Typography>
+                    </MenuItem>,
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                            <Link to='/dashboard'>Dashboard</Link>
+                        </Typography>
+                    </MenuItem>
+                    ]}
+                </Menu>
+            </Box>
+            </Toolbar>
+        </Container>
+        </AppBar>
+    );
+}
+export default Navbar
---
+++ src/config/auth0.config.ts
@@ -0,0 +1,4 @@
+export const auth0Config = {
+    domain: "dev-2pxih4mdqnkxddyf.us.auth0.com",
+    clientId: "CohfSTMOp3eASbHUgJzbuVnCYhYZbfix",
+  };
